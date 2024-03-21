8ea2f3f428d9b4feb1ac612edbe2ad7184f79010
saka1029 <saka1029@gmail.com>
2024-03-21 10:53:34 +0900

    ..

--- dentaku.bat
+++ dentaku.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 set DIR=%USERPROFILE%/git/dentaku/target
-set JAR=%DIR%/dentaku-1.0-SNAPSHOT-jar-with-dependencies.jar
+set JAR=%DIR%/dentaku-1.0-jar-with-dependencies.jar
 java -jar %JAR%
 endlocal
